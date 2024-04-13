76a49bbb6cd772f0cf37e8d71a73b42380de8df7
saadazghour <saad.azghour@gmail.com>
2024-04-13 18:13:10 +0000

    fix SensorList table UI with increased padding && font size for better readability

--- src/components/SensorList/SensorList.js
+++ src/components/SensorList/SensorList.js
@@ -12,10 +12,10 @@ function SensorList() {
   }, [dispatch]);
 
   return (
-    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-      <div className="p-4 bg-white">
-        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
+    <div className="overflow-x-auto shadow-md sm:rounded-lg">
+      <div className="p-12 bg-white">
+        <table className="w-full text-xl text-left text-gray-500">
+          <thead className="text-xl text-gray-700 uppercase bg-gray-50 ">
             <tr>
               <th scope="col" className="px-6 py-3">
                 Sensor Name
@@ -37,10 +37,10 @@ function SensorList() {
                 key={sensor.id}
                 className="bg-white border-b hover:bg-gray-50 "
               >
-                <td className="px-6 py-4">{sensor.name}</td>
-                <td className="px-6 py-4">{sensor.type}</td>
-                <td className="px-6 py-4">{sensor.location}</td>
-                <td className="px-6 py-4">{sensor.status}</td>
+                <td className="px-6 py-6">{sensor.name}</td>
+                <td className="px-6 py-6">{sensor.type}</td>
+                <td className="px-6 py-6">{sensor.location}</td>
+                <td className="px-6 py-6">{sensor.status}</td>
               </tr>
             ))}
           </tbody>
