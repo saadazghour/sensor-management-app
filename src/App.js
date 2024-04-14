9d7404aff0ca90603fe518edb315f6f63542666f
saadazghour <saad.azghour@gmail.com>
2024-04-14 05:21:22 +0100

    feat(App): Integrate React Router && enhanced navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SensorList from "../src/components/SensorList/SensorList";
+import SensorForm from "../src/components/SensorForm/SensorForm";
+
 import logo from "./logo.svg";
 import "./App.css";
-import SensorList from "../src/components/SensorList/SensorList";
 
 function App() {
   return (
-    <div className="">
-      <SensorList />
-    </div>
+    <Router>
+      <div className="">
+        <Routes>
+          <Route path="/" element={<SensorList />} />
+          <Route path="/add-sensor" element={<SensorForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
